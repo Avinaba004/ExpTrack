0bfb33fa6a2162695bd530dd7fc5c72e1faa4090
kingisLive2004 <bhattacharyasamik0206@gmail.com>
2025-10-25 05:03:26 +0530

    Describe your changes here

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function DashboardPage() {
+  return <div className="px-5">
+    {/* Budget Progress */}
+
+    {/* Overview */}
+
+    {/* Account Grid */}
+  </div>;
+}
+
+export default DashboardPage;
